c487f6f7150bb389d1aeadc1eeaba8c19b495cfe
Rahul Thakare <rahulthakare1504@gmail.com>
2018-05-15 16:35:28 +0530

    Added setting file and README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RandomColorScheme
-RandomColorScheme for Sublime Text
+RandomColorScheme plugin for Sublime Text 2/3
+
+This plugin offers a great way to handle changing Sublime Text's color-scheme to a random one very quickly.
+
+### Settings:
+`"do_not_include_packages": []`
+
+This setting can be used to avoid setting random color-scheme from specific packages.
+
+`"do_not_include_filenames": []`
+
+This setting avoids setting the color-scheme of specified filenames.
+
+### Thanks:
+This plugin is highly inspired from an already present plugin by [jugyo](https://github.com/jugyo/SublimeColorSchemeSelector).
--- RandomColorScheme.py
+++ RandomColorScheme.py
@@ -1,4 +1,5 @@
 # This package is greatly inspired from https://packagecontrol.io/packages/ColorSchemeSelector
+
 import sublime
 import sublime_plugin
 import random
@@ -7,6 +8,15 @@ class RandomColorSchemeCommand(sublime_plugin.WindowCommand):
     def run(self, **kwargs):
         if int(sublime.version()) > 3000:
             color_schemes = sublime.find_resources("*.tmTheme")
+
+            exclude_packages_list = list(map(self.exclude_package, color_schemes))
+            exclude_packages_list = list(filter(lambda v: v is not None, exclude_packages_list))
+
+            exclude_files_list = list(map(self.exclude_file, color_schemes))
+            exclude_files_list = list(filter(lambda v: v is not None, exclude_files_list))
+
+            color_schemes = self.remove_duplicates(exclude_packages_list, color_schemes)
+            color_schemes = self.remove_duplicates(exclude_files_list, color_schemes)
         else:
             color_schemes = [
                 "All Hallow's Eve",
@@ -33,18 +43,38 @@ class RandomColorSchemeCommand(sublime_plugin.WindowCommand):
                 "iPlastic"
             ]
 
-        random.shuffle(color_schemes)
+        # From: https://stackoverflow.com/a/306417/5490303
+        secure_random = random.SystemRandom()
+        color_scheme = secure_random.choice(color_schemes)
+        self.set_color_scheme(color_scheme)
 
-        self.set_color_scheme(color_schemes.pop())
+    def exclude_package(self, scheme):
+        for package in self.get_setting('do_not_include_packages'):
+            if scheme.startswith('Packages/' + package + '/'):
+                return scheme
+
+    def exclude_file(self, scheme):
+        for filename in self.get_setting('do_not_include_filenames'):
+            if filename in scheme:
+                return scheme
+
+    def remove_duplicates(self, remove, list):
+        for item in remove:
+            while list.count(item) > 0:
+                list.remove(item)
+        return list
 
     def set_color_scheme(self, color_scheme_path):
-        self.load_settings().set('color_scheme', color_scheme_path)
+        self.load_preferences().set('color_scheme', color_scheme_path)
         sublime.save_settings('Preferences.sublime-settings')
         sublime.status_message('RandomColorScheme: ' + color_scheme_path)
-        print('')
-        print('RandomColorScheme: ' + color_scheme_path)
-        print('')
+        print("\n[RandomColorScheme: Setting '" + color_scheme_path + "']\n")
 
-    def load_settings(self):
+    def load_preferences(self):
         return sublime.load_settings('Preferences.sublime-settings')
 
+    def get_settings(self):
+        return sublime.load_settings('RandomColorScheme.sublime-settings')
+
+    def get_setting(self, setting):
+        return self.get_settings().get(setting)
